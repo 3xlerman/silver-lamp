3d31ab3615614dc1908879b73e1a3f4264d4373c
Valeriy <lermanworks@gmail.com>
2020-05-17 18:26:44 +0300

    contact page template added

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var homeTemplate *template.Template
+var homeTemplate, contactTemplate *template.Template
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
@@ -19,7 +19,9 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:lermanworks@gmail.com\">lermanworks@gmail.com")
+	if err := contactTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 }
 
 func faq(w http.ResponseWriter, r *http.Request) {
@@ -34,10 +36,17 @@ func notfound(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	var err error
+
 	homeTemplate, err = template.ParseFiles("views/home.gohtml")
 	if err != nil {
 		panic(err)
 	}
+
+	contactTemplate, err = template.ParseFiles("views/contact.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
